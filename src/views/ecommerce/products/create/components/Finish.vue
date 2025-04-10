1e34f170f677f4bc3a0d29701f60e2fa517ccd03
dibidibooo <alaska.lubit.tebya@gmail.com>
2025-04-10 09:52:33 +0500

    Edit

--- src/views/ecommerce/products/create/components/Finish.vue
+++ src/views/ecommerce/products/create/components/Finish.vue
@@ -4,8 +4,8 @@
     <b-col lg="6">
       <div class="text-center">
         <i class="bx bx-check-double text-success h2"></i>
-        <h3 class="mt-0">Успешно !</h3>
-        <h5 class="w-75 mb-2 mt-3 mx-auto text-muted">Ваш товар был успешно добавлен!</h5>
+        <h3 class="mt-0">Отлично!</h3>
+        <h5 class="w-75 mb-2 mt-3 mx-auto text-muted">Перед сохранением, пожалуйста проверьте введенную информацию при необходимости</h5>
       </div>
     </b-col>
   </b-row>
