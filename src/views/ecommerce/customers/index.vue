c8560e2dbbcc6a892398ef8484cfd0546c68336e
GDK1995 <gulminez_95@mail.ru>
2025-04-25 11:14:44 +0500

    excel and list of authors

--- src/views/ecommerce/customers/index.vue
+++ src/views/ecommerce/customers/index.vue
@@ -5,7 +5,7 @@
 
     <div class="tab-content pt-0">
       <div class="tab-pane" id="team-list" :class="customersTab && 'show active'">
-        <List />
+        <List :authorList="authorList"/>
       </div>
 
       <div class="tab-pane" id="team-grid" :class="!customersTab && 'show active'">
@@ -16,16 +16,33 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted, inject } from 'vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import PageBreadcrumb from '@/components/PageBreadcrumb.vue'
 import List from '@/views/ecommerce/customers/components/List.vue'
 import Grid from '@/views/ecommerce/customers/components/Grid.vue'
 import { customerList } from '@/views/ecommerce/customers/components/data'
 
+const axios: any = inject('axios')
 
+const authorList = ref<any[]>([])
 const customersTab = ref(true)
 const perPageItem = ref(5)
 const currentPage = ref(1)
 
+async function getSeller () {
+  const token = JSON.parse(sessionStorage.getItem('QAZAQART_VUE_USER') || '{}')
+  axios.defaults.headers.common.Authorization = `Bearer  ${token?.token}`
+  await axios.get('https://dbqazaqart.kz/api/get-sellers/')
+    .then((response: { data: any }) => {
+      authorList.value = response.data
+    })
+    .catch((error: { data: any }) => {
+      console.log('<>', error)
+    })
+}
+
+onMounted(() => {
+  getSeller()
+})
 </script>
